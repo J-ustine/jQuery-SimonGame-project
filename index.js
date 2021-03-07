e76043007bf3ca80bc050d381d5da142d2da3e26
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-03-07 12:16:24 +0100

    Show sequence to the user with animation + check which button is pressed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+let userClickedPattern = [];
+let gamePattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
+let randomChosenColor = buttonColors[nextSequence()];
 
+function nextSequence() {
+  let randomNumber = null;
+  randomNumber = Math.round(Math.random() * 3);
+  return randomNumber;
+}
+
+function addRandomColorToGame() {
+  gamePattern.push(`${randomChosenColor}`);
+}
+addRandomColorToGame();
+let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+
+$(".btn").click(function (event) {
+  let userChosenColor = null;
+  userChosenColor = event.currentTarget.classList[1];
+  userClickedPattern.push(userChosenColor);
+});
+
+$(`#${randomChosenColor}`).click(function () {
+  $(`#${randomChosenColor}`).fadeOut(50);
+  $(`#${randomChosenColor}`).fadeIn(50);
+  audio.play();
+});
